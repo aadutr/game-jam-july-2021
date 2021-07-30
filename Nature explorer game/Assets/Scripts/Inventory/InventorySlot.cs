b2af2c561c2e29fabb2fd080c2ee8e9d7cf69b75
aadutr <anouk.dutree@dutchanalytics.com>
2021-07-30 20:57:50 +0200

    Made inventory items clickable via keys

--- Nature explorer game/Assets/Scripts/Inventory/InventorySlot.cs
+++ Nature explorer game/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,11 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class InventorySlot : MonoBehaviour
 {
     public Image icon;
+    public KeyCode key;
+    private Button button;
+    private DialogueTrigger dialogTrig;
     
     Item item;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        dialogTrig = GetComponent<DialogueTrigger>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            // Click the button
+            FadeToColor(button.colors.pressedColor);
+            button.onClick.Invoke();
+        }
+        else if (Input.GetKeyUp(key))
+        {
+            FadeToColor(button.colors.normalColor);
+        }
+    }
+
+    void FadeToColor(Color color)
+    {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(color, button.colors.fadeDuration, true, true);
+    }
 
     public void AddItem ( Item newItem)
     {
@@ -28,6 +58,7 @@ public class InventorySlot : MonoBehaviour
         if (item != null)
         {
             item.Use();
+            dialogTrig.TriggerDialogue();
         }
     }
 }
