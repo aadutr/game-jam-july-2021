b436ab0eaa78f34622e9ab61b0b355438b9e8eb1
aadutr <anouk.dutree@dutchanalytics.com>
2021-07-31 11:05:17 +0200

    fixed the scriptable object references

--- Nature explorer game/Assets/Scripts/Items/Thistle.cs
+++ Nature explorer game/Assets/Scripts/Items/Thistle.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Thistle", menuName = "Inventory/Thistle")]
 public class Thistle : Item
 {
     private string bird = "goldFinch";
 	private PlayerController player;
 
 
-	public void OnEnable(){
+	public void Start(){
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 	}
 
